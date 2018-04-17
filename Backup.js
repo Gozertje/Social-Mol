2bba713e0816004272d68ed73e79289fa103cbcd
Nouw <nouw@nouw.xyz>
2018-04-17 19:53:59 +0200

    Navigation

---
+++ Backup.js
@@ -0,0 +1,13 @@
+	//Dropdown
+    
+    <div class="dropdown">
+		<button class="dropbtn"><i class="fa fa-hdd-o"></i>Hardware
+			<i class="fa fa-caret-down"></i>
+		</button>
+		<div class="dropdown-content">
+			<a href="Hardware.html">#</a>
+			<a href="Hardware.html">#</a>
+			<a href="Hardware.html">#</a>
+			<a href="Hardware.html">#</a>
+		</div>
+	</div>
